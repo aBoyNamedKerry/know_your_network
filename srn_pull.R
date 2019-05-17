dabac63f55931363c79f4c3ff02e57619c5c53d6
kerry cella <kerry.cella@gmail.com>
2019-05-17 12:16:20 +0100

    added basic leaflet map of Brimingham srn and add game and game data from "https://github.com/pssguy/epldata/tree/master/data"

--- srn_pull.R
+++ srn_pull.R
@@ -17,9 +17,26 @@ View(srn)
 manchester_srn<- srn %>% filter(AUTHO_NAME == "Manchester")
 birmingham_srn<- srn %>% filter(AUTHO_NAME == "Birmingham")
 
+write_sf(birmingham_srn, "./birmingham_srn")
 
 plot(manchester_srn)
 plot(birmingham_srn)
 
+#transform data
+birmingham_srn<- st_transform(birmingham_srn, crs = "+init=epsg:4326")
+
+
+##build leaflet map ----
+
+srn_pop <- paste0("Road Number: ",
+                  birmingham_srn$ROA_NUMBER,
+                  "<br>",
+                  "Location: ",
+                  birmingham_srn$LOCATION)
+
+leaflet(birmingham_srn) %>%
+  addProviderTiles(providers$CartoDB.Positron)%>%
+  addPolygons(stroke = 0.4, color = "black", fillOpacity = 0.1,
+              popup = ~srn_pop) 
 
 
