75c229d0d50c65ec6c4decfa0b0f1fa9731e7e3b
Jane <mei-chen.chen@kcl.ac.uk>
2019-05-18 05:53:58 +0100

    all-nighter #yay
    add the third tab (evaluation), which is a calender-ish view of all the events including planned works coming up

--- know_your_network/app.R
+++ know_your_network/app.R
@@ -16,7 +16,9 @@ library(tidyverse)
 library(magrittr)
 library(janitor)
 library(DT) 
+library(png)
 
+logo <- readPNG('../Data/kyn.png')
 srn<- st_read("../Outputs/birmingham_srn.shp")
 #srn <- st_read("./Data/network.shp")
 events <- read_csv("../Data/events_next_week_birmingham.csv")
@@ -24,105 +26,145 @@ source("api_call.R")
 #traffic_A38M <- read.csv('../Data/A38(M)_traffic.csv', skip = 3)
 traffic_A5 <- read.csv('../Data/A5_traffic.csv', skip = 3)
 traffic_M6 <- read.csv('../Data/M6_traffic.csv', skip = 3)
+planned_works <- read.csv('../Data/planned_works.csv')
+colnames(planned_works) <- c('startDate', 'startTime', 'endDate', 'endTime', 'workType', 'description')
 
 # Define UI for application that draws a histogram
+DBheader <- dashboardHeader(title = "Know your network!")
+DBheader$children[2]$children <- tags$a(tags$img(src = 'logo.png', height = '60', width = '60'))
+
 ui <- dashboardPage(skin = "blue",
 
-    dashboardHeader(title = "Know your network!"),
+                    DBheader,
                     
-        dashboardSidebar(
-            sidebarMenu(
-                menuItem("Events", tabName = "events", icon = icon("dashboard")),
-                menuItem('Analysis', tabName = 'analysis', icon = icon('bar-chart-o'))
-            )
-        ), # end of dashboard sidebar
+                    dashboardSidebar(
+                        sidebarMenu(
+                            menuItem("Events", tabName = "events", icon = icon("dashboard")),
+                            menuItem('Analysis', tabName = 'analysis', icon = icon('bar-chart-o')), 
+                            menuItem('Evaluation', tabName = 'evaluation', icon = icon('calendar'))
+                        )
+                    ), # end of dashboard sidebar
                     
-        dashboardBody(
-            tabItems(
-                tabItem(tabName = "events",
-                fluidPage(
-                    # Application title
-                    titlePanel("Event planner"),
-                    # Sidebar with a selectInput 
-                    sidebarLayout(
-                        sidebarPanel(
-                            h3("Choose event date range"),
-                            #data range in
-                            dateRangeInput(inputId = "date_range", label = "Date Range", 
-                                start = min(events$startDate), 
-                                end = max(events$startDate)),
-                                      
-                            selectInput(inputId = "segment", label = "Select segment", 
-                                        choices =  srn$SECT_LABEL, 
-                                        selected =  srn$SECT_LABEL[1]
-                                            ),
-                            selectInput(inputId = "hour", label = "Select hour",
-                                        choices = c(0,1:23), selected = 12)
-                                   
-                        ), # end of sidebarPanel
-                        
-                        # Show a plot of the map
-                        mainPanel(
-
-                            box(leafletOutput("map"), width = 12, height = "420px"), 
-
-
-                            box(column(dataTableOutput("events_table"),
-                                       width = 12),
-                                width = 12)
-                            
-                        )# end main panel
-                        
-                    )# end side panel
-                    
-                )# end fluid page
-                
-            ), #end of tabitem
+                    dashboardBody(
+                        tabItems(
+                            tabItem(tabName = "events",
+                            fluidPage(
+                                # Application title
+                                titlePanel("Event planner"),
+                                # Sidebar with a selectInput 
+                                sidebarLayout(
+                                    sidebarPanel(
+                                        h3("Choose event date range"),
+                                        #data range in
+                                        dateRangeInput(inputId = "date_range", label = "Date Range", 
+                                            start = min(events$startDate), 
+                                            end = max(events$startDate)),
+                                                  
+                                        selectInput(inputId = "segment", label = "Select segment", 
+                                                    choices =  srn$SECT_LABEL, 
+                                                    selected =  srn$SECT_LABEL[1]
+                                                        ),
+                                        selectInput(inputId = "hour", label = "Select hour",
+                                                    choices = c(0,1:23), selected = 12)
+                                               
+                                    ), # end of sidebarPanel
+                                    
+                                    # Show a plot of the map
+                                    mainPanel(
             
-            tabItem(tabName = 'analysis',
-
-                fluidRow(
-
-                    # tab/page title
-                    titlePanel('Analysis'),
-
-                    sidebarLayout(
-
-                        sidebarPanel(
-
-                            h3('Select day of the week. '),
-
-                            # data input
-                            selectInput(inputId = 'segment2', label = 'Select Segment',
-                                        choices = c('A5', 'M6')),
-
-                            selectInput(inputId = 'weekday', label = 'Weekday',
-                                        choices = c('Monday' = 0, 'Tuesday' = 1, 'Wednesday' = 2,
-                                                    'Thursday' = 3, 'Friday' = 4, 'Saturday' = 5,
-                                                    'Saunday' = 6),
-                                        selected = 0,
-                                        multiple = TRUE),
-              
-                            sliderInput(inputId = 'time', label = 'Time',
-                                        min = 0, max = 24, value = c(9, 11), post = ':00')
-
-                        ),
-
-                        mainPanel(
-
-                            plotOutput('traffic_flow')
-        
-                        ) # end of sidebarPanel
-        
-                    ) # end of sidebarLayout
-        
-                ) # end of fluidPage
-        
-            ) # end of tabItem
+                                        box(leafletOutput("map"), width = 12, height = "420px"), 
+            
+            
+                                        box(column(dataTableOutput("events_table"),
+                                                   width = 12),
+                                            width = 12)
+                                        
+                                    )# end main panel
+                                    
+                                )# end side panel
                                 
-        ) # end of tabitems
-           
-    ) # dashboard body
+                            )# end fluid page
+                            
+                        ), #end of tabitem
+                        
+                        tabItem(tabName = 'analysis',
+            
+                            fluidPage(
+            
+                                # tab/page title
+                                titlePanel('Analysis'),
+            
+                                sidebarLayout(
+            
+                                    sidebarPanel(
+            
+                                        h3('Select day of the week. '),
+            
+                                        # data input
+                                        selectInput(inputId = 'segment2', label = 'Select Segment',
+                                                    choices = c('A5', 'M6')),
+            
+                                        selectInput(inputId = 'weekday', label = 'Weekday',
+                                                    choices = c('Monday' = 0, 'Tuesday' = 1, 'Wednesday' = 2,
+                                                                'Thursday' = 3, 'Friday' = 4, 'Saturday' = 5,
+                                                                'Saunday' = 6),
+                                                    selected = 0,
+                                                    multiple = TRUE),
+                          
+                                        sliderInput(inputId = 'time', label = 'Time',
+                                                    min = 0, max = 24, value = c(9, 11), post = ':00')
+            
+                                    ),
+            
+                                    mainPanel(
+            
+                                        plotOutput('traffic_flow')
+                    
+                                    ) # end of sidebarPanel
+                    
+                                ) # end of sidebarLayout
+                    
+                            ) # end of fluidPage
+                    
+                        ), # end of tabItem
+                        
+                        tabItem(tabName = 'evaluation',
+                                
+                            fluidPage(
+                              
+                              # tab/page title
+                              titlePanel('Evaluation'),
+                              
+                              sidebarLayout(
+                                  
+                                  sidebarPanel(
+                                      
+                                      h3('Select the Date.'),
+                                      
+                                      # data input
+                                      dateInput(inputId = "cal_date", label = "Date Range", 
+                                                value = min(events$startDate), 
+                                                format = 'dd-mm-yyyy', 
+                                                datesdisabled = FALSE)
+                                      
+                                  ),
+                                  
+                                  mainPanel(
+                                    
+                                      box(column(dataTableOutput('calendar'), width = 12), 
+                                          width = 12)
+                                    
+                                  ) # end of sidebarPanel
+                                  
+                                ) # end of sidebarLayout
+                              
+                            ) # end of fluidPage
+                                
+                        ) # end of tabItem
+                                            
+                    ) # end of tabitems
+                       
+                ) # dashboard body
 
 ) # End of dashboard page
 
@@ -132,10 +174,10 @@ server <- function(input, output) {
 
     #create reactive events object
     events_react<- reactive({
-  
+    
         df<- events %>% filter(startDate>= input$date_range[1],
                                startDate<= input$date_range[2])
-    
+        
         # df<- get_events(date_from = input$date_range[1],
         #                 date_to = input$date_range[2])
         # df %>%
@@ -144,9 +186,9 @@ server <- function(input, output) {
         # df
         #
     })
-
+    
     output$map <- renderLeaflet({
-  
+    
         # generate bins based on input$bins from ui.R
         srn_pop <- paste0("Road Number: ",
                          srn$ROA_NUMBER,
@@ -156,7 +198,7 @@ server <- function(input, output) {
                          "<br>",
                          "Location: ",
                          srn$LOCATION)
-    
+        
         srn_col<- colorFactor(c("red", "green"), as.factor(srn$Jan_01))
         m <- leaflet(srn) %>%
             addProviderTiles(providers$CartoDB.Positron)%>%
@@ -166,39 +208,52 @@ server <- function(input, output) {
         m %>%
             addMarkers(lng=events_react()$venue.location.lon, lat=events_react()$venue.location.lat,
                        popup=paste0(events_react()$headline, "<br>", events_react()$startDate))
-     })
-  
-     output$events_table<- renderDataTable ({
-  
-         events_react() %>% select(headline, startDate, venue.id) %>%
-         datatable()
-     })
-  
-     output$traffic_flow <- renderPlot({
-         # select segment
-         if (input$segment2 == 'A5'){
-           data = traffic_A5
-         } else {
-           data = traffic_M6
-         }
+    })
     
-         # obtaining days
-         id = input$weekday
+    output$events_table<- renderDataTable ({
     
-         # plot barchart of number of vehicles every 15 minutes
-         selected_day <- data[data$Day.Type.ID %in% id, ]
-         aggr <- aggregate(selected_day$Total.Carriageway.Flow, list(selected_day$Local.Time), mean)
-         colnames(aggr) <- c('time_of_day', 'traffic_flow')
-         attach(aggr)
-         barplot(traffic_flow, width = .25, space = 0, names.arg = time_of_day, xlim = c(0, 24))
-    
-         start_time = input$time[1]
-         end_time = input$time[2]
-         abline(v = start_time, col = 'blue', lwd = 2)
-         abline(v = end_time, col = 'blue', lwd = 2)
-     })
+        events_react() %>% select(headline, startDate, venue.id) %>%
+        datatable()
+    })
+  
+    output$traffic_flow <- renderPlot({
+        # select segment
+        if (input$segment2 == 'A5'){
+        data = traffic_A5
+        } else {
+        data = traffic_M6
+        }
+        
+        # obtaining days
+        id = input$weekday
+        
+        # plot barchart of number of vehicles every 15 minutes
+        selected_day <- data[data$Day.Type.ID %in% id, ]
+        aggr <- aggregate(selected_day$Total.Carriageway.Flow, list(selected_day$Local.Time), mean)
+        colnames(aggr) <- c('time_of_day', 'traffic_flow')
+        attach(aggr)
+        barplot(traffic_flow, width = .25, space = 0, names.arg = time_of_day, xlim = c(0, 24))
+        
+        start_time = input$time[1]
+        end_time = input$time[2]
+        abline(v = start_time, col = 'blue', lwd = 2)
+        abline(v = end_time, col = 'blue', lwd = 2)
+    })
+     
+    output$calendar <- renderDataTable({
+        
+        events_on_the_day <- events %>% filter(startDate <= input$cal_date & endDate >= input$cal_date) %>%
+            select(startDate, startTimeString, endDate, endTimeString, title, description) %>%
+            as.data.frame()
+        works_on_the_day <- planned_works %>% filter(startDate <= input$cal_date & endDate >= input$cal_date) %>% 
+            select(startDate, startTime, endDate, endTime, workType, description) %>%
+            as.data.frame()
+        all_on_the_day <- as.data.frame(rbind(events_on_the_day, works_on_the_day))
+        # colnames(all_on_the_day) <- c('Start Date', 'Start Time', 'End Date', 'End Time', 'Event Title/Work Type', 'Description')
+        
+    })
 
- }
+}
 
 
 # Run the application 
