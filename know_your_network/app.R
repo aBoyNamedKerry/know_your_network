d752172fa41def4d4f69d61b35699696dccedd96
Jane <jane841106@gmail.com>
2019-05-18 02:58:30 +0100

    just an push

--- know_your_network/app.R
+++ know_your_network/app.R
@@ -26,104 +26,104 @@ traffic_M6 <- read.csv('../Data/M6_traffic.csv', skip = 3)
 # Define UI for application that draws a histogram
 ui <- dashboardPage(skin = "blue",
                     
-                    dashboardHeader(title = "Know your network!"),
-                    
-                    dashboardSidebar(
-                      sidebarMenu(
-                        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")), 
-                        menuItem('Analysis', tabName = 'analysis', icon = icon('bar-chart-o'))
-                      )
-                                       ), # end of dashboard sidebar
-                    
-                    dashboardBody(
-                     
-                        tabItems(
-                          
-                          tabItem(tabName = 'dashboard', 
-                                  
-                            fluidPage(
-                                  
-                                  # Application title
-                                  titlePanel("Event planner"),
-                                  
-                                  
-                                  # Sidebar with a selectInput 
-                                  sidebarLayout(
-                                    
-                                    sidebarPanel(
-                                       h3("Choose event date range"),
-                                      
-                                      #data range input
-                                      dateRangeInput(inputId = "data_range", label = "Date Range", 
-                                                     start = "2018-06-01", end = "2018-08-31"),
-                                      
-                                      selectInput(inputId = "segment", label = "Select segment", 
-                                                  choices =  srn$ROA_NUMBER
-                                                      )
-                                   
-                                    ), # end of sidebarPanel
-                                    
-                                    # Show a plot of the map
-                                    mainPanel(
-                                     # splitLayout(cellWidths = c("60%", "40%"),
-                                                  
-                                      leafletOutput("map"),
-                                      
-                                      dataTableOutput("events_table")
-                                      #)#split layout end
-                                    
-                                    )# end main panel
-                                  
-                                  )# end side panel
-                                
-                                )# end fluid page
-                          
-                              ),# end tabItem
-                        
-                          tabItem(tabName = 'analysis', 
-                                   
-                            fluidRow(
-                            
-                              # tab/page title
-                              titlePanel('Analysis'),
+  dashboardHeader(title = "Know your network!"),
+  
+  dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")), 
+    menuItem('Analysis', tabName = 'analysis', icon = icon('bar-chart-o'))
+  )
+                   ), # end of dashboard sidebar
+  
+  dashboardBody(
+  
+    tabItems(
+      
+      tabItem(tabName = 'dashboard', 
+              
+        fluidPage(
+              
+              # Application title
+              titlePanel("Event planner"),
+              
+              
+              # Sidebar with a selectInput 
+              sidebarLayout(
+                
+                sidebarPanel(
+                   h3("Choose event date range"),
+                  
+                  #data range input
+                  dateRangeInput(inputId = "data_range", label = "Date Range", 
+                                 start = "2018-06-01", end = "2018-08-31"),
+                  
+                  selectInput(inputId = "segment", label = "Select segment", 
+                              choices =  srn$ROA_NUMBER
+                                  )
+               
+                ), # end of sidebarPanel
+                
+                # Show a plot of the map
+                mainPanel(
+                 # splitLayout(cellWidths = c("60%", "40%"),
                               
-                              sidebarLayout(
-                                
-                                sidebarPanel(
-                                  
-                                  h3('Select day of the week. '),
-                                  
-                                  # data input
-                                  selectInput(inputId = 'segment2', label = 'Select Segment', 
-                                              choices = c('A5', 'M6')),
-                                  
-                                  selectInput(inputId = 'weekday', label = 'Weekday', 
-                                              choices = c('Monday' = 0, 'Tuesday' = 1, 'Wednesday' = 2, 
-                                                          'Thursday' = 3, 'Friday' = 4, 'Saturday' = 5, 
-                                                          'Saunday' = 6), 
-                                              selected = 0, 
-                                              multiple = TRUE),
-                                  
-                                  sliderInput(inputId = 'time', label = 'Time', 
-                                              min = 0, max = 24, value = c(9, 11), post = ':00')
-                                  
-                                ), 
-                                
-                                mainPanel(
-                                  
-                                  plotOutput('traffic_flow')
-                                  
-                                ) # end of sidebarPanel
-                                
-                              ) # end of sidebarLayout
-                              
-                            ) # end of fluidPage
-                            
-                          ) # end of tabItem
-                          
-                        ) # end tabItems
-                   
-                    )# dashboard body
+                  leafletOutput("map"),
+                  
+                  dataTableOutput("events_table")
+                  #)#split layout end
+                
+                )# end main panel
+              
+              )# end side panel
+            
+            )# end fluid page
+      
+          ),# end tabItem
+    
+      tabItem(tabName = 'analysis', 
+               
+        fluidRow(
+        
+          # tab/page title
+          titlePanel('Analysis'),
+          
+          sidebarLayout(
+            
+            sidebarPanel(
+              
+              h3('Select day of the week. '),
+              
+              # data input
+              selectInput(inputId = 'segment2', label = 'Select Segment', 
+                          choices = c('A5', 'M6')),
+              
+              selectInput(inputId = 'weekday', label = 'Weekday', 
+                          choices = c('Monday' = 0, 'Tuesday' = 1, 'Wednesday' = 2, 
+                                      'Thursday' = 3, 'Friday' = 4, 'Saturday' = 5, 
+                                      'Saunday' = 6), 
+                          selected = 0, 
+                          multiple = TRUE),
+              
+              sliderInput(inputId = 'time', label = 'Time', 
+                          min = 0, max = 24, value = c(9, 11), post = ':00')
+              
+            ), 
+            
+            mainPanel(
+              
+              plotOutput('traffic_flow')
+              
+            ) # end of sidebarPanel
+            
+          ) # end of sidebarLayout
+          
+        ) # end of fluidPage
+        
+      ) # end of tabItem
+      
+    ) # end tabItems
+  
+  )# dashboard body
                     
 )# End of dashboard page
 
